443e0a86efaff1d970a57ed9b84f1289e8015a12
Danielle Byrne <DanielleByrne421@gmail.com>
2021-04-26 21:27:25 -0700

    forgot to install express I think

---
+++ server.js
@@ -0,0 +1,40 @@
+
+const express = require("express");
+const app = express();
+// const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("client/build"));
+
+app.get("/api/config", (req, res) => {
+  res.json({ success: true });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.get("/api/config", (req, res) => {
+  res.json({ success: true });
+});
+
+// mongoose
+//   .connect(process.env.DB_URI || "mongodb://localhost/woudygames", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Successfully connected to database");
+//   })
+//   .catch((err) => {
+//     console.log("Unable to connect to database.");
+//     console.log(err);
+//   });
+
+app.listen(PORT, () => {
+  console.log(`Express server running on http://localhost:${PORT}`);
+});
